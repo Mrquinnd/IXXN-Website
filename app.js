75c1b89a5c1f7f063766736b4fe0a1ed3941d9b8
Quinn_Dacre <q.dacre@gmail.com>
2022-10-11 00:57:10 +1300

    More JS being added with more layout stylings.

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-$(document).ready(function(){
-    $(".hamburger").click(function(){
-      $(this).toggleClass("is-active");
-    });
-  });
+
+document.getElementById("myScene").appendChild( renderer.domElement );
+
   
